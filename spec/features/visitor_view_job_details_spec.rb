746854a2a632945c30371c2ce65d1be55bbdc21c
Silvio/Diego <poscampusmm01@Campuss-Mac-mini-5.local>
2016-07-07 20:56:28 -0300

    Refactoring Visitante vê detalhes de uma vaga

--- spec/features/visitor_view_job_details_spec.rb
+++ spec/features/visitor_view_job_details_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 feature 'Visitor visits job details' do
   scenario 'successfully' do
-    job = Job.create(title: 'Vaga de Dev',
-                     category: 'Desenvolvedor',
+    job = Job.create(title: 'Vaga de QA',
+                      category: 'Desenvolvedor',
                      company: 'Campus Code',
                      description: 'Dev Junior Rails com ao menos um projeto',
                      location: 'São Paulo')
 
     visit root_path
 
-    click_on 'Vaga de Dev'
+    click_on job.title
 
     expect(page).to have_content job.title
     expect(page).to have_content job.category
