9f2f9f2f4bb5c8dc97c69cf036bda79dacab0d12
Silvio/Diego <silvio.luiz@gmail.com>
2016-07-11 20:09:05 -0300

    Usuário edita vaga

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,6 +3,22 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def edit
+    @job= Job.find(params[:id])
+    @companies = Company.all
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_parameters)
+      redirect_to @job
+    else
+      flash[:notice] = "Não foi possível atualizar a vaga"
+      @companies = Company.all
+      render :edit
+    end
+  end
+
   def new
     @job = Job.new
     @companies = Company.all
@@ -16,7 +32,7 @@ class JobsController < ApplicationController
     elsif
       flash[:notice] = "Não foi possível criar a vaga"
       @companies = Company.all
-      render :new      
+      render :new
     end
   end
 
