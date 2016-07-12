a681a9f487f6ae298904c3168e3abb2b7d40084e
Silvio/Diego <silvio.luiz@gmail.com>
2016-07-12 20:17:24 -0300

    Refatoração controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,30 +1,26 @@
 class JobsController < ApplicationController
+  before_action :set_collections, only: [:new, :edit]
+  before_action :find_job, only: [:show, :edit, :update]
+
   def show
-    @job = Job.find(params[:id])
   end
 
   def edit
-    @job= Job.find(params[:id])
-    @companies = Company.all
-    @categories = Category.all
   end
 
   def update
-    @job = Job.find(params[:id])
+    find_job
     if @job.update(job_parameters)
       redirect_to @job
     else
       flash[:notice] = "Não foi possível atualizar a vaga"
-      @companies = Company.all
-      @categories = Category.all
+      set_collections
       render :edit
     end
   end
 
   def new
     @job = Job.new
-    @companies = Company.all
-    @categories = Category.all
   end
 
   def create
@@ -34,13 +30,22 @@ class JobsController < ApplicationController
       redirect_to @job
     elsif
       flash[:notice] = "Não foi possível criar a vaga"
-      @companies = Company.all
-      @categories = Category.all
+      set_collections
       render :new
     end
   end
 
   private
+
+  def find_job
+    @job = Job.find(params[:id])
+  end
+
+  def set_collections
+    @companies = Company.all
+    @categories = Category.all
+  end
+
   def job_parameters
     params.require(:job)
       .permit(:title, :location, :category_id,
